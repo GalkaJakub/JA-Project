ac456d92f3b7c396511a5cc04cafa6be8199fa15
GalkaJakub <kubabb2002@gmail.com>
2024-11-19 14:29:42 +0100

    ver 0.2

--- JaProj/JAAsm/JAAsm.asm
+++ JaProj/JAAsm/JAAsm.asm
@@ -1,4 +1,50 @@
+filterPixel macro
+;Here will be code for sharpening every pixel
+ endm
+
 .code
-MyProc1 proc
-MyProc1 endp
+; ASMSharpen(unsigned char output[] - RSI, unsigned char input[] - RDI, int imageWidth - R8, int pixelIndex - R9, short int sharpenMask[] - RSP+40)
+
+
+ASMSharpen proc EXPORT
+; Set RSI as a pointer to the output data
+ mov RSI, RCX
+; Set RDI as a pointer to the input data
+ mov RDI, RDX
+; Clear ymm4 and ymm5 (used for storing intermediate results)
+ vpxor ymm4, ymm4, ymm4
+ vpxor ymm5, ymm5, ymm5
+; Adjust input and output pointers by the pixel index
+ add rdi, r9
+ add rsi, r9
+; Load the image width (in bytes, width * 3 since one pixel = 3 bytes) into RAX
+ mov rax, 3
+ mul r8
+; Move RDI to the pixel in the top-left corner relative to the current pixel
+ sub rdi, rax
+ sub rdi, 3
+; Load the first element of the sharpening mask into RBX
+ mov rbx, qword ptr [rsp+40];
+; Perform sharpening for three pixels in the first row using the macro
+ filterPixel
+ filterPixel
+ filterPixel
+; Move RDI to the beginning of the next row
+ sub rdi, 9
+ add rdi, rax
+ ; Perform sharpening for three pixels in the second row
+ filterPixel
+ filterPixel
+ filterPixel
+ ; Move RDI to the beginning of the third row
+ sub rdi, 9
+ add rdi, rax
+ ; Perform sharpening for three pixels in the third row
+ filterPixel
+ filterPixel
+ filterPixel
+ 
+ ; Return from the function
+ ret ; return
+ASMSharpen endp
 end
